8e0e91765c27d19be48372aab323a857c7b7f8ab
Robert Audi <robert.audii@gmail.com>
2015-12-20 23:15:39 +0100

    Add sparc formula

---
+++ Formula/sparc.rb
@@ -0,0 +1,20 @@
+class Sparc < Formula
+  desc "Sparklines for your shell ▄ ▇ ▂ ▁ ▅ ▃"
+  homepage "http://0x49.0x2a.xyz/RobertAudi/sparc"
+  url "http://0x49.0x2a.xyz/RobertAudi/sparc/repository/archive.zip"
+  head "http://0x49.0x2a.xyz/RobertAudi/sparc.git"
+
+  version "1.0.0"
+  sha256 "afe4e56ef553dd3130cac67d249fa9f78e1d0cc9a184f3c9117bae1cf8db9780"
+
+  def install
+    system "make"
+    bin.install "sparc"
+    bin.install "sparkl"
+  end
+
+  test do
+    system "sparc",  *(10.times.each_with_object([]) { |_, a| a << rand((0.0)..(100.0)).round(2).to_s })
+    system "sparkl", *(10.times.each_with_object([]) { |_, a| a << rand((0.0)..(100.0)).round(2).to_s })
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 Homewbrew formulae
 ==================
+
+Custom [Homebrew tap].
+
+[Homebrew tap]: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/brew-tap.md
+
+Usage
+-----
+
+Tap this repository:
+
+```sh
+brew tap RobertAudi/formulae http://0x49.0x2a.xyz/RobertAudi/homebrew-formulae.git
+```
+
+Install the packages it contains like any other Homebrew package:
+
+```sh
+brew install sparc
+```
