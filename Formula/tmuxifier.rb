d20d0e022d05845b062b0a9ca29ba6a7cbe71821
Robert Audi <robert.audii@gmail.com>
2016-07-30 15:54:27 +0200

    fixup! Add tmuxifier formula

--- Formula/tmuxifier.rb
+++ Formula/tmuxifier.rb
@@ -2,11 +2,14 @@ class Tmuxifier < Formula
   desc "Powerful session, window & pane management for Tmux."
   homepage "https://github.com/jimeh/tmuxifier"
 
+  url "https://github.com/jimeh/tmuxifier/archive/v0.12.2.tar.gz"
+  sha256 "a5ed6001f242e5cf27f702691828dd03124ea8cc1269569cad61c043ee949e82"
+
   head "https://github.com/jimeh/tmuxifier.git"
 
-  depends_on "tmux"
-
   bottle :unneeded
+
+  depends_on "tmux"
 
   def install
     bin.install "bin/tmuxifier"
