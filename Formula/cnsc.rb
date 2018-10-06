d55851b187ab994e08f0aa85655e8e51883230b8
Robert Audi <robert.audii@gmail.com>
2018-10-06 21:45:38 +0200

    Add the cnsc formula

---
+++ Formula/cnsc.rb
@@ -0,0 +1,14 @@
+class Cnsc < Formula
+  desc "Angular commits minus scope and stuff."
+  homepage "https://github.com/RobertAudi/cnsc"
+  head "https://github.com/RobertAudi/cnsc.git"
+
+  def install
+    system "make", "all", "PREFIX=#{prefix}"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/cnsc" "-h"
+  end
+end
