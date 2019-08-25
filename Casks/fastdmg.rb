4eb4053ac340faa1bc88fd63aba377d2ea480d96
Robert Audi <robert.audii@gmail.com>
2019-08-25 17:04:17 +0200

    Add casks for FastDMG and IconScanner

---
+++ Casks/fastdmg.rb
@@ -0,0 +1,10 @@
+cask "fastdmg" do
+  version "1.0.1"
+  sha256 "9faf7a10fc33b8f0a1dd952328df6602128ad44e21b15a457b37c0288b302e0f"
+
+  url "https://sveinbjorn.org/files/software/FastDMG.zip"
+  name "FastDMG"
+  homepage "https://sveinbjorn.org/fastdmg"
+
+  app "FastDMG.app"
+end
